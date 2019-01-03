ee0f99a01c2ee524366fe6ad28de130d250cc528
Gabor Pihaj <gabor.pihaj@gmail.com>
2019-01-03 15:23:42 +0000

    Modify deploy.sh to be able to depyloy from gocd

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 set -xe
-kubectl apply -f kubernetes/mysql.yml
-kubectl apply -f kubernetes/web.yml
+
+echo "Deploying pet-db..."
+kubectl apply -f kubernetes/mysql.yml --namespace default
+
+IMAGE_VERSION=${GO_PIPELINE_LABEL:-latest}
+PROJECT_ID=${GCLOUD_PROJECT_ID:-devops-workshop-gabor}
+echo "Deploying pet-web image version: $IMAGE_VERSION"
+
+cat kubernetes/web.yml | sed "s/\(image: \).*$/\1us.gcr.io\/$PROJECT_ID\/pet-app:$IMAGE_VERSION/" | kubectl apply -f - --namespace default
